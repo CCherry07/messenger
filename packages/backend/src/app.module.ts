42e6c509bee784cb06b514d27a78798cc5688ca7
CCherry07 <2405693142@qq.com>
2023-05-25 12:40:42 +0800

    feat: env file

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import { __DEV__ } from 'content';
+import { resolve } from 'path';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: __DEV__ ? resolve(process.cwd(), '../../.dev.env') : resolve(process.cwd(), '../../.prod.env')
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { __DEV__ } from 'content';
+console.log(`Running in ${__DEV__ ? 'development' : 'production'} mode`);
+console.log(`Listening on port ${process.env.BACKEND_PORT || 3000}`);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.BACKEND_PORT || 3000);
 }
 bootstrap();
