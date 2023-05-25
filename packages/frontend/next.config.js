d2ecff0157761b0381112c717bc610c6c5ce8447
CCherry07 <2405693142@qq.com>
2023-05-25 21:41:14 +0800

    feat: work change

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { resolve } = require("node:path");
+
+const __DEV__ = process.env.NODE_ENV === "development";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const envConfig = require("dotenv").config({
+  path: __DEV__
+    ? resolve(process.cwd(), "../../.dev.env")
+    : resolve(process.cwd(), "../../.prod.env"),
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -8,6 +20,9 @@ const nextConfig = {
       "avatars.githubusercontent.com",
       "lh3.googleusercontent.com",
     ],
+  },
+  env: {
+    ...envConfig.parsed,
   },
 };
 
--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
