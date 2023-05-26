e61c89b94032ae0100eda22145fc580a1621b78c
CCherry07 <2405693142@qq.com>
2023-05-26 14:59:45 +0800

    feat: common Format

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { __DEV__ } from 'content';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import FormatResponse from './common/FormatResponse';
+import FormatError from './common/FormatError';
 console.log(`Running in ${__DEV__ ? 'development' : 'production'} mode`);
 console.log(`Listening on port ${process.env.BACKEND_PORT || 3000}`);
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalInterceptors(new FormatResponse());
+  app.useGlobalFilters(new FormatError());
   // docs
   const options = new DocumentBuilder()
     .setTitle('标题')
