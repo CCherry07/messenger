f67a021662a76b3460bd229a7e0b6f9c5c772352
CCherry07 <2405693142@qq.com>
2023-05-27 11:29:06 +0800

    feat: add som hook with users page

---
+++ packages/frontend/hooks/useRoutes.ts
@@ -0,0 +1,37 @@
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { HiChat } from "react-icons/hi";
+import { HiArrowLeftOnRectangle, HiUsers } from "react-icons/hi2";
+import { signOut } from "next-auth/react";
+import useConversation from "./useConversation";
+const useRoutes = () => {
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+  const routes = useMemo(
+    () => [
+      {
+        name: "Chat",
+        href: "/conversation",
+        icon: HiChat,
+        active: pathname === "/conversation" || !!conversationId,
+      },
+      {
+        name: "Users",
+        href: "/users",
+        icon: HiUsers,
+        active: pathname === "/users",
+      },
+      {
+        name: "Logout",
+        href: "#",
+        icon: HiArrowLeftOnRectangle,
+        onClick: () => signOut(),
+      },
+    ],
+    [conversationId, pathname]
+  );
+
+  return useMemo(() => ({ routes }), [routes]);
+};
+
+export default useRoutes;
