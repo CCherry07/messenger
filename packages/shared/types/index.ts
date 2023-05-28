010da8216272493e57ad7da4fe026824ffa9a1f2
CCherry07 <2405693142@qq.com>
2023-05-28 11:53:17 +0800

    chore: import type

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -1,6 +1,6 @@
-export { User as UserEntity } from "@messenger/backend/src/user/entities/user.entity";
-export { Message as EntityEntity } from "@messenger/backend/src/message/entities/message.entity";
-export { Conversation as ConversationEntity } from "@messenger/backend/src/conversation/entities/conversation.entity";
+export type { User as UserEntity } from "@messenger/backend/src/user/entities/user.entity";
+export type { Message as EntityEntity } from "@messenger/backend/src/message/entities/message.entity";
+export type { Conversation as ConversationEntity } from "@messenger/backend/src/conversation/entities/conversation.entity";
 
 export type GetPromiseValue<T> = T extends Promise<infer U> ? U : T;
 
