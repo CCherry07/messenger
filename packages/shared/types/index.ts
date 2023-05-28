93759be930afd174d0feb3f19acf9fbc9b406314
CCherry07 <2405693142@qq.com>
2023-05-28 10:23:04 +0800

    feat: create shared packages

---
+++ packages/shared/types/index.ts
@@ -0,0 +1,12 @@
+export { User as UserEntity } from "@messenger/backend/src/user/entities/user.entity";
+export { Message as EntityEntity } from "@messenger/backend/src/message/entities/message.entity";
+export { Conversation as ConversationEntity } from "@messenger/backend/src/conversation/entities/conversation.entity";
+
+export type GetPromiseValue<T> = T extends Promise<infer U> ? U : T;
+
+export type User = {
+  name: string;
+  email: string;
+  image: string;
+  accessToken: string;
+};
