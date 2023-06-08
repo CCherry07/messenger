5605ca8271eb9a9bca13af61ffea886c6cb6e6e7
CCherry07 <2405693142@qq.com>
2023-06-08 20:19:29 +0800

    feat: avaterGroup

--- packages/frontend/app/components/AvaterGroup.tsx
+++ packages/frontend/app/components/AvaterGroup.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
 import { EntitiesTypes } from "shared/types";
 
-const AvaterGroup = ({ users }: { users: EntitiesTypes["UserEntity"][] }) => {
-  return <div></div>;
+interface AvaterGroupProps {
+  users: EntitiesTypes["UserEntity"][];
+}
+
+const AvaterGroup = ({ users }: AvaterGroupProps) => {
+  const slice = users.slice(0, 3);
+  const rest = users.length - 3;
+  const positionMap: Record<number, string> = {
+    0: "top-0 left-[12px]",
+    1: "bottom-0",
+    2: "bottom-0 right-0",
+  };
+  return (
+    <div className="relative h-11 w-11">
+      {slice.map((user, index) => (
+        <div
+          key={user.id}
+          className={`absolute inline-block rounded-full overflow-hidden h-[21px] w-[21px]
+          ${positionMap[index]}
+        `}
+        >
+          <Image
+            src={
+              user.image ||
+              "https://avatars.githubusercontent.com/u/79909910?v=4"
+            }
+            fill
+            alt="avatar"
+          />
+        </div>
+      ))}
+      {rest > 0 && (
+        <div className="h-11 w-11 rounded-full absolute bottom-0 right-0 bg-gray-300 flex justify-center items-center">
+          <span className="text-sm text-gray-600 font-semibold">+{rest}</span>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AvaterGroup;
